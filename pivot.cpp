e7902fc9048118c7f67476e3603b444a8a79de0c
Srivastavarachit01 <rachitsrivastava1845@gmail.com>
2026-06-06 00:11:27 +0530

    pivo solution

---
+++ pivot.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0, leftSum = 0;
+        
+        for (int x : nums) total += x;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            // rightSum = total - leftSum - nums[i]
+            if (leftSum == total - leftSum - nums[i])
+                return i;
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+};
