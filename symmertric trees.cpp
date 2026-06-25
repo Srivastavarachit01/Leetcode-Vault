a241fdf15fc19734c84cbac5fb186d21708e923b
Rachit Srivastava <rachitsrivastava1845@gmail.com>
2026-06-25 23:39:39 +0530

    Symmetric Trees solution

---
+++ symmertric trees.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isMirror(TreeNode* l, TreeNode* r) {
+        if (!l && !r) return true;
+        if (!l || !r) return false;
+        return (l->val == r->val) 
+            && isMirror(l->left, r->right) 
+            && isMirror(l->right, r->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left, root->right);
+    }
+};
